fe87b30a2106472560eabe801c5ae747972392fc
hkodes <hishore.kumar@btech.christuniversity.in>
2021-06-06 00:56:49 -0700

    first commit

---
+++ lib/data/book_data.dart
@@ -0,0 +1,63 @@
+import 'package:filter_listview_example/model/book.dart';
+
+final allBooks = <Book>[
+  Book(
+    id: 1,
+    author: 'Work',
+    title: 'John Doe',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 2,
+    author: 'Work',
+    title: 'Leslie',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 3,
+    author: 'Family',
+    title: 'Jorden',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 4,
+    author: 'Family',
+    title: 'Rick',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 5,
+    author: 'Work',
+    title: 'Lee',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 6,
+    author: 'Family',
+    title: 'Chris',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 7,
+    author: 'Work',
+    title: 'Doe',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 8,
+    author: 'Family',
+    title: 'Carlos',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+    id: 9,
+    author: 'Work',
+    title: 'Matt',
+    urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png',
+  ),
+  Book(
+      id: 10,
+      author: 'Family',
+      title: 'Morsia',
+      urlImage: 'https://i.dlpng.com/static/png/7172810_preview.png'),
+];
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:filter_listview_example/page/filter_local_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Contacts';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MainPage(),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: buildPages(),
+      );
+
+  Widget buildPages() {
+    switch (index) {
+      case 0:
+        return FilterLocalListPage();
+
+      default:
+        return Container();
+    }
+  }
+}
